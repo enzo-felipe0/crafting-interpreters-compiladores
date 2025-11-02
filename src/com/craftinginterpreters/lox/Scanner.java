12421dda8cb02cbe4a9d4ab417abf20dbca065b9
Elias66Fernandes <francisco.elias@discente.ufma.br>
2025-11-01 21:40:29 -0300

    Implementa a leitura de literais de string

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -1,9 +1,7 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static com.craftinginterpreters.lox.TokenType.*;
 
@@ -92,10 +90,30 @@ class Scanner {
             case '\n':
                 line++;
                 break;
+            case '"': string(); break;
             default:
                 Lox.error(line, "Unexpected character.");
                 break;
         }
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        // The closing ".
+        advance();
+
+        // Trim the surrounding quotes.
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
     }
 
     private boolean match(char expected) {
