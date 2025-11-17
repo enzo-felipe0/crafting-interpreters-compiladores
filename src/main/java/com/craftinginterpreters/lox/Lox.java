c8988909a66f7da64d31068b3152b1158dc0ca47
enzo <enzofelipe2525@gmail.com>
2025-11-17 20:57:36 -0300

    fix(Lox): retirando exibição de dados desnecessários deixando somente a AST

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -52,12 +52,6 @@ public class Lox {
         if (hadError) return;
 
         System.out.println(new AstPrinter().print(expression));
-
-
-        // For now, just print the tokens.
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
     }
 
     static void error(int line, String message) {
