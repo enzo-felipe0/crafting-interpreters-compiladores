d4dff8f304b049d1771f93c1e72c0dceb04366a4
Elias66Fernandes <francisco.elias@discente.ufma.br>
2025-11-21 02:20:35 -0300

    fix: Implementa a função runtimeError e define códigos de saída

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -10,6 +10,8 @@ import java.util.List;
 
 public class Lox {
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -27,6 +29,7 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
 
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -72,4 +75,11 @@ public class Lox {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
     }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
+    }
+
 }
